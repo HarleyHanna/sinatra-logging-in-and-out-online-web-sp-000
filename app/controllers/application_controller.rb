31dba8de146e2f4bf8f646dd46d41198ed1c250d
Harley Hanna <harleyhannawrit@gmail.com>
2020-11-16 13:34:59 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,13 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    @user = User.find(session[:user_id])
+    @user = User.find_by(session[:user_id])
+    if @user && @user.password == params[:password]
+      session[:user_id] = @user.id
+      redirect to '/account'
+    else
+      erb :error
+    end
   end
 
   get '/account' do
