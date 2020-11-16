c505e0dac7e2841224ebe72f58a6b4e19c2631b3
Harley Hanna <harleyhannawrit@gmail.com>
2020-11-16 13:47:04 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    @uaer = User.find_by(session[:user_id])
+    @uaer = User.find_by_id(session[:user_id])
     if :user
       erb :account
     else
